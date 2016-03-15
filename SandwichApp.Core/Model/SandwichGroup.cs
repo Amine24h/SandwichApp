98fe2f0acdd56e92a9a03995067952f123ade965
amine24h <amine.caius@gmail.com>
2016-03-15 21:26:27 +0100

    first commit

---
+++ SandwichApp.Core/Model/SandwichGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amine24hSandwich.Core.Model
+{
+    class SandwichGroup
+    {
+        public int SandwichGroupId { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public List<Sandwich> Sandwiches { get; set; }
+    }
+}
---
+++ SandwichApp.Core/Service/SandwishDataService.cs
@@ -0,0 +1,40 @@
+﻿using Amine24hSandwich.Core.Model;
+using Amine24hSandwich.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amine24hSandwich.Core.Service
+{
+    class SandwishDataService
+    {
+        private static SandwichRepository sandwichRepository = new SandwichRepository();
+
+        public List<Sandwich> GetAllSandwiches()
+        {
+            return sandwichRepository.GetAllSandwiches();
+        }
+
+        public List<SandwichGroup> GetGroupedSandwiches()
+        {
+            return sandwichRepository.GetGroupedSandwiches();
+        }
+
+        public List<Sandwich> GetSandwichesForGroup(int SandwishGroupId)
+        {
+            return sandwichRepository.GetSandwichesForGroup(SandwishGroupId);
+        }
+
+        public List<Sandwich> GetFavoriteSandwiches()
+        {
+            return sandwichRepository.GetFavoriteSandwiches();
+        }
+
+        public Sandwich GetSandwishById(int SandwishId)
+        {
+            return sandwichRepository.GetSandwichById(SandwishId);
+        }
+    }
+}
